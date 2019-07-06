b47dfb00d93b0eebc55a2b9fe861b3d35cf3a7d6
Luis Fernando Zarazua Aguilar <spartanzarazua117@hotmail.com>
2019-07-05 22:40:48 -0500

    Texto cambiado y 2 archivos html ananidos

---
+++ codigo.js
@@ -0,0 +1,9 @@
+alert("Si funciona el codigo.js");
+var ruta=window.location;//location es el nombre de un atributo de window.
+var documento=document;
+var ventana=window;
+document.write("Hola estudiante<br/>");//document es un objeto.
+document.write("Estás en: " + ruta.origin);
+console.log(ruta);
+console.log(documento);
+console.log(ventana);
